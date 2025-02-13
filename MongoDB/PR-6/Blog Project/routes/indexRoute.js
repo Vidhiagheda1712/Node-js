712c556b49990ea87abe27c13119607a8f9c8983
VidhiAgheda <168967567+Vidhiagheda1712@users.noreply.github.com>
2025-02-13 11:54:22 +0530

    Delete MongoDB/PR-6/Blog Project directory

--- MongoDB/PR-6/Blog Project/routes/indexRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const routes = express.Router();
-
-routes.use('/', require('./authRoute'));
-routes.use('/', require('./blogRoute'));
-
-
-module.exports = routes;
