e02c82a0b5dc952cd1b930f48a1d99d2b5ba0794
Vidhiagheda1712 <aghedavidhi1712@gmail.com>
2025-02-22 17:07:36 +0530

    Node-work

--- MongoDB/Admin Panel/controllers/subCategoryController.js
+++ MongoDB/Admin Panel/controllers/subCategoryController.js
@@ -1,16 +1,28 @@
+const CategoryModel =require('../models/categoryModel')
 const SubCategoryModel = require('../models/subCategoryModel');
+const addsubCategoryPage = async(req, res) => {
+    try{
+          let subcategory =await CategoryModel.find({status:'active'})
+          return res.render('subcategory/add_subcategory',{
+             category:subcategory
+          })
+    }
+    catch (err) {
+        console.log(err);
+        return false;
 
-const addsubCategoryPage = (req, res) => {
-    return res.render('subcategory/add_subCategory')
+    }
+
 
 }
 const insertsubCategory = async (req, res) => {
     try {
-        const { category } = req.body;
-        const add = await SubCategoryModel.create({
-            category: category
+        const { category ,subcategory } = req.body;
+        const sub = await SubCategoryModel.create({
+            categoryId: category,
+            subcategory:subcategory
         })
-        req.flash('success', 'Category Add Successfully...')
+        req.flash('success', 'SubCategory Add Successfully...')
         return res.redirect('/category/addcategorypage');
 
 
