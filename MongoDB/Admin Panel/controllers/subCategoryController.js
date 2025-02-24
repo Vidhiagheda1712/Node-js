65a8d0bab333e3e8cc618d55747b86037d1b19f8
Purvivanpariya <purvipatel4503@gmail.com>
2025-02-24 11:38:32 +0530

    Node-Work

--- MongoDB/Admin Panel/controllers/subCategoryController.js
+++ MongoDB/Admin Panel/controllers/subCategoryController.js
@@ -1,11 +1,11 @@
-const CategoryModel =require('../models/categoryModel')
+const CategoryModel = require('../models/categoryModel');
 const SubCategoryModel = require('../models/subCategoryModel');
-const addsubCategoryPage = async(req, res) => {
-    try{
-          let subcategory =await CategoryModel.find({status:'active'})
-          return res.render('subcategory/add_subcategory',{
-             category:subcategory
-          })
+const addsubCategoryPage = async (req, res) => {
+    try {
+        let subcategory = await CategoryModel.find({ status: 'active' })
+        return res.render('subcategory/add_subcategory', {
+            category: subcategory
+        })
     }
     catch (err) {
         console.log(err);
@@ -13,14 +13,14 @@ const addsubCategoryPage = async(req, res) => {
 
     }
 
-
 }
+
 const insertsubCategory = async (req, res) => {
     try {
-        const { category ,subcategory } = req.body;
+        const { category, subcategory } = req.body;
         const sub = await SubCategoryModel.create({
             categoryId: category,
-            subcategory:subcategory
+            subcategory: subcategory
         })
         req.flash('success', 'SubCategory Add Successfully...')
         return res.redirect('/category/addcategorypage');
@@ -32,4 +32,38 @@ const insertsubCategory = async (req, res) => {
 
     }
 }
-module.exports = { addsubCategoryPage ,insertsubCategory }
+const viewSubcategorypage = async (req, res) => {
+    try {
+        let subcategory = await SubCategoryModel.find({ status: 'active' }).populate('categoryId');
+        return res.render('subcategory/view_subCategory', {
+            subcategory: subcategory,
+
+        })
+    }
+    catch (err) {
+        console.log(err);
+        return false;
+
+    }
+}
+
+const editSubcategory = async (req, res) => {
+    try {
+        const id = req.query.id;
+        let category = await CategoryModel.find({ status: 'active' });
+        let single = await SubCategoryModel.findById(id).populate('categoryId');
+        return res.render('subcategory/edit_subCategory',{
+            single:single,
+            category:category
+        })
+
+
+
+    }
+    catch (err) {
+        console.log(err);
+        return false;
+
+    }
+}
+module.exports = { addsubCategoryPage, insertsubCategory, viewSubcategorypage, editSubcategory }
--- MongoDB/Admin Panel/routes/subCategoryRoute.js
+++ MongoDB/Admin Panel/routes/subCategoryRoute.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const routes = express.Router();
- 
-const{addsubCategoryPage,insertsubCategory} = require('../controllers/subCategoryController')
+
+const { addsubCategoryPage, insertsubCategory, viewSubcategorypage, editSubcategory } = require('../controllers/subCategoryController')
 
 routes.get('/addsubcategorypage', addsubCategoryPage);
-routes.post('/insertsubcategory',insertsubCategory);
-
-
-
-
+routes.get('/', viewSubcategorypage);
+routes.post('/insertsubcategory', insertsubCategory);
+routes.get('/editsubcategory', editSubcategory)
 module.exports = routes;
