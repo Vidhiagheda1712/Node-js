79ddd368f1d1a2b574236c7e6c742d8e668f3d4e
Vidhiagheda1712 <aghedavidhi1712@gmail.com>
2025-03-06 11:51:24 +0530

    Node-Work

---
+++ Express-JS/API/Lecture-2/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const port = 9000;
+const app = express();
+const db = require('./config/db');
+
+// const cors =require('cors');
+// app.use(cors());
+
+app.use(express.urlencoded());
+app.use('/',require('./routes/indexRoute'))
+app.listen(port, (err) => {
+     if(err){
+        console.log(err);
+        return false;
+        
+     }
+     console.log(`Server is Start on Port :-${port}`);
+     
+})
---
+++ Express-JS/API/Lecture-2/routes/authRoute.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const routes = express.Router();
+const { registerUser, loginUser } = require('../controller/authController');
+
+routes.post('/register', registerUser);
+routes.post('/login', loginUser);
+module.exports = routes;
