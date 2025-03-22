b582e135d42d7724fcc63f5a71e705c4e5297e10
VidhiAgheda <168967567+Vidhiagheda1712@users.noreply.github.com>
2025-03-22 19:51:50 +0530

    Delete NodeExam2025 directory

--- NodeExam2025/routes/indexRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const routes = express.Router();
-
-routes.use('/', require('./authRoute'));
-routes.use('/', require('./productRoute'));
-
-
-module.exports = routes;
